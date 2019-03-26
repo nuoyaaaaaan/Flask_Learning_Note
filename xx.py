6e5e8604d8ed1382fc051ce20aadc3eb76969afe
260168 <1060322010@qq.com>
2019-03-26 14:57:28 +0800

    add xx.py

---
+++ xx.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+'''
+Create on 19-3-23
+
+@auther: 260168
+
+@requirements: PyCharm 2017.2.4, Python 3.6
+'''
+
+def func1():
+    pass
+
+print(func1)
+print(func1.__name__)
+
+func1.xxx = 123
+print(func1.xxx)
+
