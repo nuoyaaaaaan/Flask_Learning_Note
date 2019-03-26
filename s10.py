edb1dcab7f9e30ea853c46d76d7ce0dd4aac41bc
260168 <1060322010@qq.com>
2019-03-26 14:55:54 +0800

    add s10.py

---
+++ s10.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+'''
+Create on 19-3-25
+
+@auther: 260168
+
+@requirements: PyCharm 2017.2.4, Python 3.6
+'''
+from flask import Flask, render_template, Markup
+app = Flask(__name__)
+app.debug = True
+
+
+def func1(arg):
+    return Markup("<input type='text' value='%s' />" %(arg,))
+
+@app.route('/')
+def index():
+    return render_template('s10index.html', ff=func1)
+
+
+if __name__ == '__main__':
+    app.run()
+
