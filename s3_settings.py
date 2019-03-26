d5760ddf82c138a7583eede5e5bc87576f072795
260168 <1060322010@qq.com>
2019-03-26 14:54:03 +0800

    add s3_settings.py

---
+++ s3_settings.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+'''
+Create on 19-3-23
+
+@auther: 260168
+
+@requirements: PyCharm 2017.2.4, Python 3.6
+'''
+
+DEBUG = True
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'sqlite://:memory:'
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
