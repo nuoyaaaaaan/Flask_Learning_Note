428483727b642be6ac896ba96b3a033d2e99b691
260168 <1060322010@qq.com>
2019-03-26 14:54:36 +0800

    add s5.py

---
+++ s5.py
@@ -0,0 +1,40 @@
+#coding=utf-8
+'''
+Create on 19-3-23
+
+@auther: 260168
+
+@requirements: PyCharm 2017.2.4, Python 3.6
+'''
+from flask import Flask, views
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+def auth(func):
+    def inner(*args, **kwargs):
+        print('before')
+        result = func(*args, **kwargs)
+        print('after')
+        return result
+    return inner
+
+
+class IndexView(views.MethodView):
+    methods = ['GET']
+    decorators = [auth, ]
+
+    def get(self):
+        return 'Index.GET'
+
+    def post(self):
+        return 'Index.POST'
+
+
+app.add_url_rule('/index', view_func=IndexView.as_view(name='index'))  # name = endpoint
+
+
+if __name__ == '__main__':
+    app.run()
