a66ec5de69c4c02725a2b89b983093c6b2e497de
260168 <1060322010@qq.com>
2019-03-26 14:54:20 +0800

    add s4.py

---
+++ s4.py
@@ -0,0 +1,45 @@
+#coding=utf-8
+'''
+Create on 19-3-23
+
+@auther: 260168
+
+@requirements: PyCharm 2017.2.4, Python 3.6
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+
+"""
+1.decorator = app.route('/', methods=["GET", "POST"], endpoint='n1')
+    def route(self, rule, **options):
+        # self = app对象
+        # rule = /
+        # options = methods=["GET", "POST"], endpoint='n1'
+        def decorator(f):
+            endpoint = options.pop('endpoint', None)
+            self.add_url_rule(rule, endpoint, f, **options)
+            return f
+        return decorator
+2.@decorator 
+    decorator(index)
+"""
+
+
+@app.route('/', methods=["GET", "POST"], endpoint='n1')
+def index():
+    return "hello world"
+
+
+def login():
+    return '登录'
+
+
+app.add_url_rule('/login', 'n2', login, methods=['GET', 'POST'])
+
+
+if __name__ == '__main__':
+    app.run()
